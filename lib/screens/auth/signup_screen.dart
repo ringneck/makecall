6c8482b3b478a710ec2f6b2a718853e26d87495b
ringneck <genspark_flutter_dev@genspark.ai>
2025-11-17 11:16:58 +0000

    fix: signup_screen.dart에서 kDebugMode import 누락 수정

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
