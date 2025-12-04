cb470c7c50a6acbb221d1d16d2b7484c8eaef223
MAKECALL Dev <makecall@olssoo.com>
2025-12-04 14:58:21 +0000

    Fix: MainScreen에서 FCMService.setContext() 호출 추가
    - MainScreen initState에서 FCMService.setContext() 호출
    - 기기 승인 다이얼로그 표시를 위한 유효한 BuildContext 확보
    - PostFrameCallback으로 안전하게 context 설정

--- lib/screens/home/main_screen.dart
+++ lib/screens/home/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../call/call_tab.dart';
+import '../../services/fcm_service.dart';
 
 class MainScreen extends StatefulWidget {
   final int? initialTabIndex; // 초기 탭 인덱스 (null이면 기본값 사용)
@@ -17,6 +18,21 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  @override
+  void initState() {
+    super.initState();
+    
+    // 🔔 FCM BuildContext 설정 (기기 승인 다이얼로그용)
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (mounted) {
+        FCMService.setContext(context);
+        if (kDebugMode) {
+          debugPrint('📺 [MainScreen] FCMService.setContext() 호출 완료');
+        }
+      }
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
     // CallTab이 신규 사용자 감지 및 ProfileDrawer 자동 열기를 처리
